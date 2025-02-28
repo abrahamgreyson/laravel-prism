1b63dfb43b22c95a59dcae28dc8e344a0ba8c1b9
abrahamgreyson <abrahamgreyson@gmail.com>
2025-02-28 15:15:48 +0800

    :sparkles: feat: snowflake and telescope

--- src/PrismServiceProvider.php
+++ src/PrismServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Abe\Prism;
 
-use Abe\Prism\Commands\PrismCommand;
+use Carbon\CarbonImmutable;
+use Closure;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -15,11 +18,30 @@ class PrismServiceProvider extends PackageServiceProvider
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
-        $package
-            ->name('laravel-prism')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_prism_table')
-            ->hasCommand(PrismCommand::class);
+        $package->name('laravel-prism')->hasConfigFile();
+    }
+
+    public function register(): void
+    {
+        // Telescope can be run only in local
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
+        // snowflake setting
+        $this->app->singleton('snowflake', function ($app) {
+            return (new \Godruoyi\Snowflake\Snowflake())
+                // the day I started rewrite this project
+                ->setStartTimeStamp(strtotime('2024-05-20') * 1000)
+                ->setSequenceResolver(new \Godruoyi\Snowflake\LaravelSequenceResolver($app->get('cache.store')));
+        });
+    }
+
+    public function boot(): void
+    {
+        // disabled resource wrapping
+        JsonResource::withoutWrapping();
+
+        // use immutable dates
+        Date::use(CarbonImmutable::class);
     }
 }
